66cd170ea2bd268a0334e4314c98ef1f946143e2
Steve Kinney <hello@stevekinney.net>
2015-02-25 09:35:44 -0700

    Begin nested routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('artists', function () {
+    this.resource('artist', { path: '/:artist_id' });
+  });
 });
 
 export default Router;
